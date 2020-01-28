cdbb191c2b8124df866c5023cf2da7288065a060
ProjectPD <59799249+userpddemo@users.noreply.github.com>
2020-01-28 13:54:27 +0530

    added Xml Ajax Function

--- flex.js
+++ flex.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-    $("#flex1").click(function(){
-        $(this).parents(".container").find("#view_code").load("flex-1.txt");
-     alert("OK");
-    })
-})
+function setElementId(var id){
+    var elementValue = document.getElementById(id).href
+}
+
+function viewCode(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("view_code").text =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", elementValue, true);
+  xhttp.send();
+}
